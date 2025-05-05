8e1edb47fc7e8287e500d0303a94f01fd46a52c1
amy <chiema2103@gmail.com>
2025-05-05 18:54:50 -0400

    stage 5 timer + refinement
    - add all members to credit page and all assets used
    - added congratulatory and thank you message for victory area
    - imported mitchell's ingame timer (changed the horizontal alignment to center so its more stable for different screen resolutions)

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,5 @@ extends Control
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/game stage.tscn")
 
-func _on_credits_pressed():
-	print("credit pressed")
-
 func _on_quit_pressed():
 	get_tree().quit()
--- scripts/victory_trigger.gd
+++ scripts/victory_trigger.gd
@@ -3,18 +3,23 @@ extends Area2D
 @onready var fireworkgreen = $fireworks/AnimatedSprite2D1
 @onready var victory_song = $AudioStreamPlayer2D
 @onready var canvas_modulate = $"../../../CanvasModulate"
+@onready var victory_message = $"../../../VictoryMessage"
+signal victory_reached
+
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		if not victory_song.playing:
 			# victory effects
+			emit_signal("victory_reached")
 			body.set_light_visibility(false)
 			canvas_modulate.color = Color(1, 1, 1)  # Return to normal
 			canvas_modulate.visible = false
 			Music.stop()
 			victory_song.play()
 			
-			#play fireworks
+			#play fireworks and display message
+			victory_message.visible = true
 			fireworkgreen.play("green")
 			
 			#wait for victory song to finish playing
