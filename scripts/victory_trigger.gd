d21da5b61fbc056634bb6524c30a1de064fb6011
mitchellflewelling207 <flewellingm2@mail.husson.edu>
2025-05-05 14:00:38 -0400

    added-running-timer

--- scripts/victory_trigger.gd
+++ scripts/victory_trigger.gd
@@ -1,19 +1,25 @@
 extends Area2D
 
-@onready var victory_song = $AudioStreamPlayer2D
-@onready var canvas_modulate = $"../../../CanvasModulate"
+signal victory_reached
+
+
+@onready var victory_song: AudioStreamPlayer2D = $VictorySong
+@onready var canvas_modulate: CanvasModulate = $"../../CanvasModulate"
+
+
+func _ready():
+	print("Victory song node: ", victory_song)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		if not victory_song.playing:
-			# victory effects
+			#victory effects
 			body.set_light_visibility(false)
-			canvas_modulate.color = Color(1, 1, 1)  # Return to normal
+			canvas_modulate.color = Color(1,1,1)
+			
 			canvas_modulate.visible = false
 			Music.stop()
 			victory_song.play()
 			
-			#wait for victory song to finish playing
 			get_tree().create_timer(4.0).timeout
-			
-			
+			emit_signal("victory_reached")
